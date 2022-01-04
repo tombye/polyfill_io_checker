eca4d4eea919bdf695d2bd794e41bd7246478d44
Tom Byers <tombaromba@gmail.com>
2022-01-04 16:38:49 +0000

    Make build produce data for bundle

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const fs = require('fs');
 const polyfillLibrary = require('polyfill-library');
 const outputFileRaw = './bundle.raw.js';
 const outputFileMinified = './bundle.min.js';
+const outputDataFile = './bundle.data.json';
+
+const uaStrings = {
+  "ie9": "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)",
+  "ie11": "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko"
+};
+
 const languageFeatures = {
   'Array.prototype.concat': {},
   'Array.prototype.filter': {},
@@ -29,28 +36,112 @@ const languageFeatures = {
   'String.prototype.trim': {}
 }
 
-const polyfillBundleRaw = polyfillLibrary.getPolyfillString({
-  uaString: 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+const defaultOptions = {
+  uaString: uaStrings.ie9,
   minify: false,
   features: languageFeatures
-}).then(function(bundleString) {
-  fs.writeFile(outputFileRaw, bundleString, error => {
-    if (error) {
-      console.log(error);
-      return;
-    }
-  });
-});
+};
 
-const polyfillBundleMinified = polyfillLibrary.getPolyfillString({
-  uaString: 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
-  minify: true,
-  features: languageFeatures
-}).then(function(bundleString) {
-  fs.writeFile(outputFileMinified, bundleString, error => {
-    if (error) {
-      console.log(error);
-      return;
-    }
+function generateBundle (outputFile, newOptions) {
+
+  let options = Object.assign({}, defaultOptions);
+
+  if (newOptions !== undefined) {
+    options = Object.assign(options, newOptions);
+  }
+
+  const polyfillBundle = polyfillLibrary.getPolyfillString(options).then(function(bundleString) {
+    fs.writeFile(outputFile, bundleString, error => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+    });
   });
-});
+
+};
+
+// Polyfills data contains Sets which need turning to Arrays to be stored as JSON
+function processPolyfillsData (data) {
+
+  Object.entries(data).forEach(entry => {
+
+    const [entryName, entryAttrs] = entry;
+
+    Object.entries(entryAttrs).forEach(attr => {
+
+      const [attrName, attrValue] = attr;
+
+      if (attrValue instanceof Set) {
+        data[entryName][attrName] = Array.from(attrValue);
+      }
+
+    });
+
+  });
+
+  return data;
+
+};
+
+function reportBundleData (data) {
+
+  const report = {
+    notRecognised: [],
+    polyfillsIncluded: [],
+    dependencies: []
+  };
+
+  Object.entries(data).forEach(entry => {
+
+    const [entryName, entryAttrs] = entry;
+
+    if (Object.keys(entryAttrs).length === 0) {
+
+      report.notRecognised.push(entryName);
+
+    } else {
+
+      if (entryAttrs.dependencyOf.length > 0) {
+
+        report.dependencies.push(entryName);
+
+      } else {
+
+        report.polyfillsIncluded.push(entryName);
+
+      }
+
+    }
+
+  });
+
+  return report;
+
+};
+
+function generatePolyfillsData (outputFile, newOptions) {
+
+  let options = Object.assign({}, defaultOptions);
+
+  if (newOptions !== undefined) {
+    options = Object.assign(options, newOptions);
+  }
+
+  const polyfillBundle = polyfillLibrary.getPolyfills(options).then(function(bundleData) {
+    fs.writeFile(outputFile, JSON.stringify(processPolyfillsData(bundleData), null, 4), error => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+    });
+
+    console.log(reportBundleData(bundleData));
+  });
+
+};
+
+generateBundle(outputFileRaw);
+generateBundle(outputFileMinified, {"minify": true});
+generatePolyfillsData(outputDataFile);
+
---
+++ test.js
@@ -0,0 +1,399 @@
+/* Polyfill service v3.109.0
+ * For detailed credits and licence information see https://github.com/financial-times/polyfill-service.
+ * 
+ * Features requested: Array.prototype.includes
+ * 
+ * - _ESAbstract.Call, License: CC0 (required by "Array.prototype.includes", "_ESAbstract.ToString", "_ESAbstract.ToPrimitive", "_ESAbstract.OrdinaryToPrimitive")
+ * - _ESAbstract.CreateMethodProperty, License: CC0 (required by "Array.prototype.includes")
+ * - _ESAbstract.Get, License: CC0 (required by "Array.prototype.includes", "_ESAbstract.ToString", "_ESAbstract.ToPrimitive", "_ESAbstract.OrdinaryToPrimitive")
+ * - _ESAbstract.IsCallable, License: CC0 (required by "Array.prototype.includes", "_ESAbstract.ToString", "_ESAbstract.ToPrimitive", "_ESAbstract.OrdinaryToPrimitive")
+ * - _ESAbstract.SameValueNonNumber, License: CC0 (required by "Array.prototype.includes", "_ESAbstract.SameValueZero")
+ * - _ESAbstract.ToObject, License: CC0 (required by "Array.prototype.includes", "_ESAbstract.ToString", "_ESAbstract.ToPrimitive", "_ESAbstract.GetMethod", "_ESAbstract.GetV")
+ * - _ESAbstract.GetV, License: CC0 (required by "Array.prototype.includes", "_ESAbstract.ToString", "_ESAbstract.ToPrimitive", "_ESAbstract.GetMethod")
+ * - _ESAbstract.GetMethod, License: CC0 (required by "Array.prototype.includes", "_ESAbstract.ToString", "_ESAbstract.ToPrimitive")
+ * - _ESAbstract.Type, License: CC0 (required by "Array.prototype.includes", "_ESAbstract.ToString", "_ESAbstract.ToPrimitive", "_ESAbstract.OrdinaryToPrimitive")
+ * - _ESAbstract.OrdinaryToPrimitive, License: CC0 (required by "Array.prototype.includes", "_ESAbstract.ToString", "_ESAbstract.ToPrimitive")
+ * - _ESAbstract.SameValueZero, License: CC0 (required by "Array.prototype.includes")
+ * - _ESAbstract.ToInteger, License: CC0 (required by "Array.prototype.includes", "_ESAbstract.ToLength")
+ * - _ESAbstract.ToLength, License: CC0 (required by "Array.prototype.includes")
+ * - _ESAbstract.ToPrimitive, License: CC0 (required by "Array.prototype.includes", "_ESAbstract.ToString")
+ * - _ESAbstract.ToString, License: CC0 (required by "Array.prototype.includes")
+ * - Array.prototype.includes, License: MIT */
+
+(function(self, undefined) {
+
+// _ESAbstract.Call
+/* global IsCallable */
+// 7.3.12. Call ( F, V [ , argumentsList ] )
+function Call(F, V /* [, argumentsList] */) { // eslint-disable-line no-unused-vars
+	// 1. If argumentsList is not present, set argumentsList to a new empty List.
+	var argumentsList = arguments.length > 2 ? arguments[2] : [];
+	// 2. If IsCallable(F) is false, throw a TypeError exception.
+	if (IsCallable(F) === false) {
+		throw new TypeError(Object.prototype.toString.call(F) + 'is not a function.');
+	}
+	// 3. Return ? F.[[Call]](V, argumentsList).
+	return F.apply(V, argumentsList);
+}
+
+// _ESAbstract.CreateMethodProperty
+// 7.3.5. CreateMethodProperty ( O, P, V )
+function CreateMethodProperty(O, P, V) { // eslint-disable-line no-unused-vars
+	// 1. Assert: Type(O) is Object.
+	// 2. Assert: IsPropertyKey(P) is true.
+	// 3. Let newDesc be the PropertyDescriptor{[[Value]]: V, [[Writable]]: true, [[Enumerable]]: false, [[Configurable]]: true}.
+	var newDesc = {
+		value: V,
+		writable: true,
+		enumerable: false,
+		configurable: true
+	};
+	// 4. Return ? O.[[DefineOwnProperty]](P, newDesc).
+	Object.defineProperty(O, P, newDesc);
+}
+
+// _ESAbstract.Get
+// 7.3.1. Get ( O, P )
+function Get(O, P) { // eslint-disable-line no-unused-vars
+	// 1. Assert: Type(O) is Object.
+	// 2. Assert: IsPropertyKey(P) is true.
+	// 3. Return ? O.[[Get]](P, O).
+	return O[P];
+}
+
+// _ESAbstract.IsCallable
+// 7.2.3. IsCallable ( argument )
+function IsCallable(argument) { // eslint-disable-line no-unused-vars
+	// 1. If Type(argument) is not Object, return false.
+	// 2. If argument has a [[Call]] internal method, return true.
+	// 3. Return false.
+
+	// Polyfill.io - Only function objects have a [[Call]] internal method. This means we can simplify this function to check that the argument has a type of function.
+	return typeof argument === 'function';
+}
+
+// _ESAbstract.SameValueNonNumber
+// 7.2.12. SameValueNonNumber ( x, y )
+function SameValueNonNumber(x, y) { // eslint-disable-line no-unused-vars
+	// 1. Assert: Type(x) is not Number.
+	// 2. Assert: Type(x) is the same as Type(y).
+	// 3. If Type(x) is Undefined, return true.
+	// 4. If Type(x) is Null, return true.
+	// 5. If Type(x) is String, then
+		// a. If x and y are exactly the same sequence of code units (same length and same code units at corresponding indices), return true; otherwise, return false.
+	// 6. If Type(x) is Boolean, then
+		// a. If x and y are both true or both false, return true; otherwise, return false.
+	// 7. If Type(x) is Symbol, then
+		// a. If x and y are both the same Symbol value, return true; otherwise, return false.
+	// 8. If x and y are the same Object value, return true. Otherwise, return false.
+
+	// Polyfill.io - We can skip all above steps because the === operator does it all for us.
+	return x === y;
+}
+
+// _ESAbstract.ToObject
+// 7.1.13 ToObject ( argument )
+// The abstract operation ToObject converts argument to a value of type Object according to Table 12:
+// Table 12: ToObject Conversions
+/*
+|----------------------------------------------------------------------------------------------------------------------------------------------------|
+| Argument Type | Result                                                                                                                             |
+|----------------------------------------------------------------------------------------------------------------------------------------------------|
+| Undefined     | Throw a TypeError exception.                                                                                                       |
+| Null          | Throw a TypeError exception.                                                                                                       |
+| Boolean       | Return a new Boolean object whose [[BooleanData]] internal slot is set to argument. See 19.3 for a description of Boolean objects. |
+| Number        | Return a new Number object whose [[NumberData]] internal slot is set to argument. See 20.1 for a description of Number objects.    |
+| String        | Return a new String object whose [[StringData]] internal slot is set to argument. See 21.1 for a description of String objects.    |
+| Symbol        | Return a new Symbol object whose [[SymbolData]] internal slot is set to argument. See 19.4 for a description of Symbol objects.    |
+| Object        | Return argument.                                                                                                                   |
+|----------------------------------------------------------------------------------------------------------------------------------------------------|
+*/
+function ToObject(argument) { // eslint-disable-line no-unused-vars
+	if (argument === null || argument === undefined) {
+		throw TypeError();
+	}
+	return Object(argument);
+}
+
+// _ESAbstract.GetV
+/* global ToObject */
+// 7.3.2 GetV (V, P)
+function GetV(v, p) { // eslint-disable-line no-unused-vars
+	// 1. Assert: IsPropertyKey(P) is true.
+	// 2. Let O be ? ToObject(V).
+	var o = ToObject(v);
+	// 3. Return ? O.[[Get]](P, V).
+	return o[p];
+}
+
+// _ESAbstract.GetMethod
+/* global GetV, IsCallable */
+// 7.3.9. GetMethod ( V, P )
+function GetMethod(V, P) { // eslint-disable-line no-unused-vars
+	// 1. Assert: IsPropertyKey(P) is true.
+	// 2. Let func be ? GetV(V, P).
+	var func = GetV(V, P);
+	// 3. If func is either undefined or null, return undefined.
+	if (func === null || func === undefined) {
+		return undefined;
+	}
+	// 4. If IsCallable(func) is false, throw a TypeError exception.
+	if (IsCallable(func) === false) {
+		throw new TypeError('Method not callable: ' + P);
+	}
+	// 5. Return func.
+	return func;
+}
+
+// _ESAbstract.Type
+// "Type(x)" is used as shorthand for "the type of x"...
+function Type(x) { // eslint-disable-line no-unused-vars
+	switch (typeof x) {
+		case 'undefined':
+			return 'undefined';
+		case 'boolean':
+			return 'boolean';
+		case 'number':
+			return 'number';
+		case 'string':
+			return 'string';
+		case 'symbol':
+			return 'symbol';
+		default:
+			// typeof null is 'object'
+			if (x === null) return 'null';
+			// Polyfill.io - This is here because a Symbol polyfill will have a typeof `object`.
+			if ('Symbol' in self && (x instanceof self.Symbol || x.constructor === self.Symbol)) return 'symbol';
+
+			return 'object';
+	}
+}
+
+// _ESAbstract.OrdinaryToPrimitive
+/* global Get, IsCallable, Call, Type */
+// 7.1.1.1. OrdinaryToPrimitive ( O, hint )
+function OrdinaryToPrimitive(O, hint) { // eslint-disable-line no-unused-vars
+	// 1. Assert: Type(O) is Object.
+	// 2. Assert: Type(hint) is String and its value is either "string" or "number".
+	// 3. If hint is "string", then
+	if (hint === 'string') {
+		// a. Let methodNames be « "toString", "valueOf" ».
+		var methodNames = ['toString', 'valueOf'];
+		// 4. Else,
+	} else {
+		// a. Let methodNames be « "valueOf", "toString" ».
+		methodNames = ['valueOf', 'toString'];
+	}
+	// 5. For each name in methodNames in List order, do
+	for (var i = 0; i < methodNames.length; ++i) {
+		var name = methodNames[i];
+		// a. Let method be ? Get(O, name).
+		var method = Get(O, name);
+		// b. If IsCallable(method) is true, then
+		if (IsCallable(method)) {
+			// i. Let result be ? Call(method, O).
+			var result = Call(method, O);
+			// ii. If Type(result) is not Object, return result.
+			if (Type(result) !== 'object') {
+				return result;
+			}
+		}
+	}
+	// 6. Throw a TypeError exception.
+	throw new TypeError('Cannot convert to primitive.');
+}
+
+// _ESAbstract.SameValueZero
+/* global Type, SameValueNonNumber */
+// 7.2.11. SameValueZero ( x, y )
+function SameValueZero (x, y) { // eslint-disable-line no-unused-vars
+	// 1. If Type(x) is different from Type(y), return false.
+	if (Type(x) !== Type(y)) {
+		return false;
+	}
+	// 2. If Type(x) is Number, then
+	if (Type(x) === 'number') {
+		// a. If x is NaN and y is NaN, return true.
+		if (isNaN(x) && isNaN(y)) {
+			return true;
+		}
+		// b. If x is +0 and y is -0, return true.
+		if (1/x === Infinity && 1/y === -Infinity) {
+			return true;
+		}
+		// c. If x is -0 and y is +0, return true.
+		if (1/x === -Infinity && 1/y === Infinity) {
+			return true;
+		}
+		// d. If x is the same Number value as y, return true.
+		if (x === y) {
+			return true;
+		}
+		// e. Return false.
+		return false;
+	}
+	// 3. Return SameValueNonNumber(x, y).
+	return SameValueNonNumber(x, y);
+}
+
+// _ESAbstract.ToInteger
+/* global Type */
+// 7.1.4. ToInteger ( argument )
+function ToInteger(argument) { // eslint-disable-line no-unused-vars
+	if (Type(argument) === 'symbol') {
+		throw new TypeError('Cannot convert a Symbol value to a number');
+	}
+
+	// 1. Let number be ? ToNumber(argument).
+	var number = Number(argument);
+	// 2. If number is NaN, return +0.
+	if (isNaN(number)) {
+		return 0;
+	}
+	// 3. If number is +0, -0, +∞, or -∞, return number.
+	if (1/number === Infinity || 1/number === -Infinity || number === Infinity || number === -Infinity) {
+		return number;
+	}
+	// 4. Return the number value that is the same sign as number and whose magnitude is floor(abs(number)).
+	return ((number < 0) ? -1 : 1) * Math.floor(Math.abs(number));
+}
+
+// _ESAbstract.ToLength
+/* global ToInteger */
+// 7.1.15. ToLength ( argument )
+function ToLength(argument) { // eslint-disable-line no-unused-vars
+	// 1. Let len be ? ToInteger(argument).
+	var len = ToInteger(argument);
+	// 2. If len ≤ +0, return +0.
+	if (len <= 0) {
+		return 0;
+	}
+	// 3. Return min(len, 253-1).
+	return Math.min(len, Math.pow(2, 53) -1);
+}
+
+// _ESAbstract.ToPrimitive
+/* global Type, GetMethod, Call, OrdinaryToPrimitive */
+// 7.1.1. ToPrimitive ( input [ , PreferredType ] )
+function ToPrimitive(input /* [, PreferredType] */) { // eslint-disable-line no-unused-vars
+	var PreferredType = arguments.length > 1 ? arguments[1] : undefined;
+	// 1. Assert: input is an ECMAScript language value.
+	// 2. If Type(input) is Object, then
+	if (Type(input) === 'object') {
+		// a. If PreferredType is not present, let hint be "default".
+		if (arguments.length < 2) {
+			var hint = 'default';
+			// b. Else if PreferredType is hint String, let hint be "string".
+		} else if (PreferredType === String) {
+			hint = 'string';
+			// c. Else PreferredType is hint Number, let hint be "number".
+		} else if (PreferredType === Number) {
+			hint = 'number';
+		}
+		// d. Let exoticToPrim be ? GetMethod(input, @@toPrimitive).
+		var exoticToPrim = typeof self.Symbol === 'function' && typeof self.Symbol.toPrimitive === 'symbol' ? GetMethod(input, self.Symbol.toPrimitive) : undefined;
+		// e. If exoticToPrim is not undefined, then
+		if (exoticToPrim !== undefined) {
+			// i. Let result be ? Call(exoticToPrim, input, « hint »).
+			var result = Call(exoticToPrim, input, [hint]);
+			// ii. If Type(result) is not Object, return result.
+			if (Type(result) !== 'object') {
+				return result;
+			}
+			// iii. Throw a TypeError exception.
+			throw new TypeError('Cannot convert exotic object to primitive.');
+		}
+		// f. If hint is "default", set hint to "number".
+		if (hint === 'default') {
+			hint = 'number';
+		}
+		// g. Return ? OrdinaryToPrimitive(input, hint).
+		return OrdinaryToPrimitive(input, hint);
+	}
+	// 3. Return input
+	return input;
+}
+
+// _ESAbstract.ToString
+/* global Type, ToPrimitive */
+// 7.1.12. ToString ( argument )
+// The abstract operation ToString converts argument to a value of type String according to Table 11:
+// Table 11: ToString Conversions
+/*
+|---------------|--------------------------------------------------------|
+| Argument Type | Result                                                 |
+|---------------|--------------------------------------------------------|
+| Undefined     | Return "undefined".                                    |
+|---------------|--------------------------------------------------------|
+| Null	        | Return "null".                                         |
+|---------------|--------------------------------------------------------|
+| Boolean       | If argument is true, return "true".                    |
+|               | If argument is false, return "false".                  |
+|---------------|--------------------------------------------------------|
+| Number        | Return NumberToString(argument).                       |
+|---------------|--------------------------------------------------------|
+| String        | Return argument.                                       |
+|---------------|--------------------------------------------------------|
+| Symbol        | Throw a TypeError exception.                           |
+|---------------|--------------------------------------------------------|
+| Object        | Apply the following steps:                             |
+|               | Let primValue be ? ToPrimitive(argument, hint String). |
+|               | Return ? ToString(primValue).                          |
+|---------------|--------------------------------------------------------|
+*/
+function ToString(argument) { // eslint-disable-line no-unused-vars
+	switch(Type(argument)) {
+		case 'symbol':
+			throw new TypeError('Cannot convert a Symbol value to a string');
+		case 'object':
+			var primValue = ToPrimitive(argument, String);
+			return ToString(primValue); // eslint-disable-line no-unused-vars
+		default:
+			return String(argument);
+	}
+}
+
+// Array.prototype.includes
+/* global CreateMethodProperty, Get, SameValueZero, ToInteger, ToLength, ToObject, ToString */
+// 22.1.3.11. Array.prototype.includes ( searchElement [ , fromIndex ] )
+CreateMethodProperty(Array.prototype, 'includes', function includes(searchElement /* [ , fromIndex ] */) {
+	'use strict';
+	// 1. Let O be ? ToObject(this value).
+	var O = ToObject(this);
+	// 2. Let len be ? ToLength(? Get(O, "length")).
+	var len = ToLength(Get(O, "length"));
+	// 3. If len is 0, return false.
+	if (len === 0) {
+		return false;
+	}
+	// 4. Let n be ? ToInteger(fromIndex). (If fromIndex is undefined, this step produces the value 0.)
+	var n = ToInteger(arguments[1]);
+	// 5. If n ≥ 0, then
+	if (n >= 0) {
+		// a. Let k be n.
+		var k = n;
+		// 6. Else n < 0,
+	} else {
+		// a. Let k be len + n.
+		k = len + n;
+		// b. If k < 0, let k be 0.
+		if (k < 0) {
+			k = 0;
+		}
+	}
+	// 7. Repeat, while k < len
+	while (k < len) {
+		// a. Let elementK be the result of ? Get(O, ! ToString(k)).
+		var elementK = Get(O, ToString(k));
+		// b. If SameValueZero(searchElement, elementK) is true, return true.
+		if (SameValueZero(searchElement, elementK)) {
+			return true;
+		}
+		// c. Increase k by 1.
+		k = k + 1;
+	}
+	// 8. Return false.
+	return false;
+});
+})
+('object' === typeof window && window || 'object' === typeof self && self || 'object' === typeof global && global || {});
